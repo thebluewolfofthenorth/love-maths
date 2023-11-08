e9f0c5cce535eeb2bc3a8fea9e4eced909296d7b
andres <andres.sovik@tv2.no>
2023-11-08 23:58:21 +0100

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,30 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+runGame("addition")
+
 })
 
 /**
  * The main game "loop", called he the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+ 
+    if(gameType=== "addition") {
+        displayAddittionQuestion(num1, num2)
+    } else {
+        alert(`unknown game type: ${gameType}`)
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 
@@ -42,8 +53,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAddittionQuestion() {
-
+function displayAddittionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractionQuestion() {
