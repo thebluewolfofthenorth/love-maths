0e19613ed1e2c01062699ef752e669e770f85f0a
andres <andres.sovik@tv2.no>
2023-11-08 23:17:57 +0100

    set up structure of game fuctions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAddittionQuestion(){
+    
+}
+
+function displaySubtractionQuestion(){
+
+}
+
+function displayMultiplayQuestion(){
+
+}
