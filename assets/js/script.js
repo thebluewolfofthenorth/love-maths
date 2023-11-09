79c74cc833cdfc509f0d7cc0cbedc33261dc182e
andres <andres.sovik@tv2.no>
2023-11-09 21:56:38 +0100

    add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ function runGame(gameType) {
         displayAddittionQuestion(num1, num2);
     } else if (gameType === "multiply"){
         displayMultiplayQuestion(num1, num2);
+    } else if (gameType === "subtract"){
+        displaySubtractionQuestion (num1, num2)
     }
     else {
         alert(`unknown game type: ${gameType}`);
@@ -76,7 +78,10 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     }else if (operator === "x") {
         return[operand1 * operand2, "multiply"];
-    }else {
+    }else if (operator === "-"){
+        return[operand1 - operand2, "subtract"];
+    }
+    else {
         alert(`unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -109,8 +114,10 @@ function displayAddittionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractionQuestion() {
-
+function displaySubtractionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplayQuestion(operand1, operand2) {
