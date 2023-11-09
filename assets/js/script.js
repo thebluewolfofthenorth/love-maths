68350d5658a90788afbb784447a5f04e1b6c95c2
andres <andres.sovik@tv2.no>
2023-11-09 22:44:48 +0100

    add division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,7 @@ runGame("addition");
 function runGame(gameType) {
 
     document.getElementById("answer-box"). value = "";
-    document.getElementById("answer-box"),focus();
+    document.getElementById("answer-box").focus();
 
 
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displayMultiplayQuestion(num1, num2);
     } else if (gameType === "subtract"){
         displaySubtractionQuestion (num1, num2)
+    } else if (gameType === "division"){
+        displayDivisionQuestion (num1, num2)
     }
     else {
         alert(`unknown game type: ${gameType}`);
@@ -91,6 +93,8 @@ function calculateCorrectAnswer() {
         return[operand1 * operand2, "multiply"];
     }else if (operator === "-"){
         return[operand1 - operand2, "subtract"];
+    }else if (operator === "/"){
+        return[operand1 / operand2, "division"];
     }
     else {
         alert(`unimplemented operator ${operator}`);
@@ -135,4 +139,12 @@ function displayMultiplayQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion (operand1, operand2) {
+    operand1 = operand1 * operand2;
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
